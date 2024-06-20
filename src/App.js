6716ae27c0269b00714c2f8530acd499e46b6892
Sergei <sergeiburyi1@gmail.com>
2024-06-20 15:45:54 +0300

    init project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import colors from './constants/colors';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants/colors.js
@@ -0,0 +1,14 @@
+const colors = [
+    { name: 'Красный', hex: '#FF0000' },
+    { name: 'Зеленый', hex: '#008000' },
+    { name: 'Синий', hex: '#0000FF' },
+    { name: 'Желтый', hex: '#FFFF00' },
+    { name: 'Голубой', hex: '#00FFFF' },
+    { name: 'Пурпурный', hex: '#FF00FF' },
+    { name: 'Оранжевый', hex: '#FFA500' },
+    { name: 'Фиолетовый', hex: '#800080' },
+    { name: 'Коричневый', hex: '#A52A2A' },
+    { name: 'Серый', hex: '#808080' }
+  ];
+  
+  export default colors;
